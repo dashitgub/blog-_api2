6aeac712beec6ad87ec2e4e30fc678de8536be1e
6Katana9 <katana.st@yandex.com>
2024-05-23 18:40:36 +0600

    .

--- post/views.py
+++ post/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters 
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from .permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer, PostListSrerializer
 
@@ -14,6 +16,15 @@ class PostViewSet(ModelViewSet):
     filterset_fields = ['author']
     search_fields = ['title']
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            self.permission_classes = [AllowAny]
+        elif self.action in ['create']:
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsOwnerOrReadOnly]
+        return [permission() for permission in self.permission_classes] 
+        
 
     def get_serializer_class(self):
         if self.action == 'list':
--- review/urls.py
+++ review/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import CommentViewSet
+from .views import CommentViewSet, toggle_like
 
 router = DefaultRouter()
 router.register('comment', CommentViewSet)
 
 urlpatterns = [
+    path('like/<int:id>/', toggle_like),
     path('', include(router.urls))
 ]
 
